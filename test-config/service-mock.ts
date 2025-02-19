9c7036ad05f62b9a509cd149d48a24374a5bb64a
Janus Code <januscodee@gmail.com>
2025-02-19 04:53:36 +0000

    Update service-mock.ts

--- test-config/service-mock.ts
+++ test-config/service-mock.ts
@@ -1,7 +1,6 @@
 import { PermissionStatus } from '@airgap/angular-core'
 
-import { newSpy } from './unit-test-helper'
-
+import { newSpy } from './unit-test-helper
 export class PermissionsServiceMock {
   public hasCameraPermission = newSpy('hasCameraPermission', Promise.resolve(PermissionStatus.GRANTED))
 }
